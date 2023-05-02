8dc7ecd751bc6514892543dcbd7df6d27df80a74
Darryl Williams <williamsdarryl330@gmail.com>
2023-05-02 19:54:04 +0000

    idk

--- config/routes.rb
+++ config/routes.rb
@@ -3,29 +3,34 @@ Rails.application.routes.draw do
 
   ##user routes
   get("/users", { controller: "users", action: "index" })
-
-  get("/users/:username", { controller: "users", action: "show" })
+  
+  get("/users/:username", { controller: "users", action: "show"})
 
   ##create user
-  get("/insert_user_record", { controller: "users", action: "create" })
+  get("/insert_user_record", { controller: "users", action: "create"})
 
   ## update user
-  get("/update_user/:the_user_id", { controller: "users", action: "update" })
+  get("/update_user/:the_user_id", { controller: "users", action: "update"})
 
-  ## photo routes
-  get("/photos", { controller: "photos", action: "index" })
+
+
+
+
+## photo routes
+  get("/photos", { controller: "photos",  action: "index" })
 
   ## create
   get("/insert_photo_record", { controller: "photos", action: "create" })
 
   ## read
-  get("/photos/:the_photo_id", { controller: "photos", action: "show" })
+  get("/photos/:the_photo_id", { controller: "photos", action: "show"})
 
   ## update
   get("/update_photo/:the_photo_id", { controller: "photos", action: "update" })
 
   ## delete
   get("/delete_photo/:the_photo_id", { controller: "photos", action: "destroy" })
+
 
   ## comments
   post("/insert_comment_record", { controller: "comments", action: "create" })

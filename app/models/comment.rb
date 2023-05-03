f4e3a32ab26ad14c828f2c1574105df6267f9df5
Darryl Williams <williamsdarryl330@gmail.com>
2023-05-03 14:13:48 +0000

    FINAL

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,7 +11,6 @@
 #
 class Comment < ApplicationRecord
   validates(:photo_id, { :presence => true })
-  validates(:body, { :presence => true })
   validates(:author_id, { :presence => true })
 
   belongs_to(:commenter, { :required => true, :class_name => "User", :foreign_key => "author_id" })
